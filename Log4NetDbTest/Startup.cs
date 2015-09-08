fc4691998f876d553c784f8c86ac2b5a7fbafcd6
CodeAndYou <codeandyouforum@gmail.com>
2015-09-08 14:24:14 +0530

    First checkin

---
+++ Log4NetDbTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Log4NetDbTest.Startup))]
+namespace Log4NetDbTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
